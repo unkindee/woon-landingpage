1b3180e2c20ab31537c888b6b7443c62d33b8e76
Dragos Bucatica <psytrance2k4@gmail.com>
2021-12-29 15:58:19 +0200

    routes sample

--- src/Routes.js
+++ src/Routes.js
@@ -2,12 +2,27 @@ import {
   Routes,
   Route,
 } from 'react-router-dom'
+import PageMetaTitle from './components/PageMetaTitle'
 import { urls } from './constants'
 import Home from './pages/Home/Home'
+import MainLayout from './partials/MainLayout'
+
+const ComingSoon = ({ title }) => (
+  <MainLayout>
+    <PageMetaTitle title={title} />
+    <div style={{ minHeight: '400px', paddingTop: '200px' }}>Coming Soon: {title}</div>
+  </MainLayout>
+)
 
 const AppRoutes = () => (
   <Routes>
     <Route path={urls.home} element={<Home />} />
+    <Route path={urls.ik_huur} element={<ComingSoon title={urls.ik_huur} />} />
+    <Route path={urls.ik_zoek} element={<ComingSoon title={urls.ik_zoek} />} />
+    <Route path={urls.projecten} element={<ComingSoon title={urls.projecten} />} />
+    <Route path={urls.contact} element={<ComingSoon title={urls.contact} />} />
+
+    <Route path="*" element={<div>Not found</div>} />
   </Routes>
 )
 
